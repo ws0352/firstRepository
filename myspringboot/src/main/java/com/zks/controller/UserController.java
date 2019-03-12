b2f2228d4b7cb31d18127b804e6f917533b8e4c7
weilining <18277278902@163.com>
2019-03-12 21:42:37 +0800

    f

--- myspringboot/src/main/java/com/zks/controller/UserController.java
+++ myspringboot/src/main/java/com/zks/controller/UserController.java
@@ -5,6 +5,8 @@ import com.zks.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.jws.soap.SOAPBinding;
+
 /**
  * Created by 韦师兄
  * 2019-03-12 14:53
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @GetMapping("/login")
     @ResponseBody
-    public MyResult login(){
+    public MyResult login(@RequestBody User user){
         MyResult result = new MyResult();
         result.setCode(0);
         result.setMsg("登录成功！");
